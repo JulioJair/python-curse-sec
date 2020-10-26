45471357c3e3f73772cd8588ff1bfb7849e36c70
Julio de Alba <juliojair@outlook.com>
2020-10-26 12:54:18 -0600

    new_code

---
+++ auto_poo.py
@@ -0,0 +1,9 @@
+class Auto:
+    def __init__(self, marc=None, colo=None, gas=None):
+        self.marca = marc
+        self.color = colo
+        self.gasolina = gas
+
+        def llenar_tanque:
+            pass
+
