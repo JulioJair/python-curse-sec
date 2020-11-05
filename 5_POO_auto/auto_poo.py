810feefda1a2b2b3deab60686c3007f20b5228e2
Siemens Mantenimiento <juliojair@outlook.com>
2020-11-04 20:28:58 -0600

    finish

--- 5_POO_auto/auto_poo.py
+++ 5_POO_auto/auto_poo.py
@@ -1,24 +1,43 @@
 class Auto:
-    def __init__(self, marca=None, color=None, gas=None):
+    def __init__(self, marca=None, color=None, gasolina=None):
         self.marca = marca
         self.color = color
-        self._gasolina = gas
+        self._gasolina = gasolina
 
-    def avanzar(self):
+    def avanzar(self, distancia, rel_gas_km=1 / 800):
+        """Distancia en km"""
         print('Avanzando')
+        gasolina_gastada = distancia * rel_gas_km
+        self._gasolina -= gasolina_gastada
 
     def detener(self):
         print('Frenando')
 
     def get_nivel_gasolina(self):
-        print(self._gasolina)
+        print(f"Nivel de gasolina: {self._gasolina * 100}%")
+
+    def _llenar_tanque(self):
+        if self._gasolina < 1:
+            self._gasolina = 1
+            print("Tanque llenado con éxito")
+        else:
+            print("El tanque ya está lleno al máximo de su capacidad!")
+
+    def __repr__(self):
+        return f"{self.marca} {self.color}\n Gasolina al {self._gasolina * 100}%"
 
 
-a1=Auto('Nissan','Gris','Lleno')
-a2=Auto('Ferrari','rojo','Lleno')
-a3=Auto('Mazda','Negro','Bajo')
-a4=Auto('BMW','Azul','Alto')
-a5=Auto('Ford','Blanco','Bajo')
-a6=Auto('Kia','Rojo','Vacío')
+if __name__ == "__main__":
+    a1 = Auto('Nissan', 'Gris', 16 / 16)
+    a2 = Auto('Ferrari', 'rojo', 15 / 16)
+    a3 = Auto('Mazda', 'Negro', 1 / 16)
+    a4 = Auto('BMW', 'Azul', 14 / 16)
+    a5 = Auto('Ford', 'Blanco', 10 / 16)
+    a6 = Auto('Kia', 'Rojo', 5 / 16)
 
-a6.get_nivel_gasolina()
+    print(a2)
+    a2.get_nivel_gasolina()
+    a2.avanzar(50)
+    a2.get_nivel_gasolina()
+    a2._llenar_tanque()
+    a2.get_nivel_gasolina()
